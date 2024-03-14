45a3b38637acf807f6777131d6847ddfb9ae8b80
FranciscoRey <franciscorey98@gmail.com>
2024-03-14 15:08:43 -0300

    Registro y manejo de errores al momento de enviar el mail

--- EnviadorMails.py
+++ EnviadorMails.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 # Conexión SQL
 import pyodbc
 # Envío de emails
@@ -7,17 +8,18 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-# Desencriptación
+# Desencriptación contraseña
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 # Conexión a la base de datos
-server = 'DESKTOP-97GRPND\SQLEXPRESS'
-database = 'ADACSCSD'
-username = 'EnviadorMails'
-password = 'test123'
+servidor = 'DESKTOP-97GRPND\\SQLEXPRESS'
+base_de_datos = 'ADACSCSD'
+usuario = 'EnviadorMails'
+contraseña = 'test123'
+
 try:
-    conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+    conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={servidor};DATABASE={base_de_datos};UID={usuario};PWD={contraseña}')
     cursor = conn.cursor()
 except Exception as e:
     print("Error al conectarse a la base de datos:", e)
@@ -42,32 +44,19 @@ try:
 except Exception as e:
     print("Error al buscar el parámetro 1304 (puerto) en PARAMETROS:", e)
 
-# Esto no está funcionando
-def decrypt_password(encoded_str):
-    decrypted_str = ""
-    for pos, char in enumerate(encoded_str):
-        xor_key = 255 - ((pos + 1) * 2) % 32
-        decrypted_char = chr(ord(char) ^ xor_key)
-        decrypted_str += decrypted_char
-    return decrypted_str
-
 try:   
-    cursor.execute("SELECT CADCRYPT FROM PARAMETROS WHERE CODIGO=1306")
+    cursor.execute("SELECT CADENA FROM PARAMETROS WHERE CODIGO=9017")
     row = cursor.fetchone()
     if row:       
-        encrypted_password = row.CADCRYPT
-        decrypted_password = decrypt_password(encrypted_password)
-        # No es la contraseña correcta parece
-        sender_password = decrypt_password
+        sender_password = row.CADENA
     else:
-        print("Contraseña no encontrada en el parámetro 1306.")
+        print("Contraseña no encontrada en el parámetro 9017.")
 except Exception as e:
-    print("Error al buscar el parámetro 1306 (contraseña) en PARAMETROS:", e)
+    print("Error al buscar el parámetro 9017 (contraseña) en PARAMETROS:", e)
 
 sender_email = 'franciscorey98@gmail.com'
 
 def send_email(destinatario, subject, body, attachment_path=None):
-
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender_email
@@ -100,6 +89,9 @@ def main():
                 email_subject = row.ASU
                 email_body = row.CUE
                 email_attachment = row.ADJ
+                #sent_datetime = datetime.datetime.now()       
+                #sent_date = sent_time.date()
+                #sent_time = sent_time.time()
                 send_email(email_destinatario, email_subject, email_body, email_attachment)
 
                 cursor.execute(f"UPDATE EMAILSLOG SET EST=1 WHERE ANR={row.ANR}")
@@ -108,12 +100,20 @@ def main():
             # Debería hacer el close acá o al salir del While?
             #conn.close()
         except Exception as e:
+            print("2 - Error al enviar el mail:", e) 
 
-            cursor.execute(f"UPDATE EMAILSLOG SET EST=12 WHERE ANR={row.ANR}")
-            conn.commit()
+            error_code = e.args[0] if e.args else None
+            if error_code == 2:
+                error_message = "Verifique la existencia del archivo adjunto."
+            else:
+                error_message = e.args[1]
 
-            print("2 - Error:", e)
-
+            try:
+                cursor.execute(f"UPDATE EMAILSLOG SET EST=?, ERRCOD=?, ERRDES=? WHERE ANR={row.ANR}", (12, error_code, error_message))     
+                conn.commit()
+            except Exception as e:
+                print("3 - Error al modificar el registro en EMAILSLOG:", e)    
+                 
         break
         time.sleep(15)
 
